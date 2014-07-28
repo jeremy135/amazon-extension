24fc22148e548f44f943015158bb52c9c0c08443
Vladimir Lezhnin <jeremy@tsjee.ru>
2014-07-29 00:08:47 +0900

    version 0.7 format money

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
-version 0.6
+==version 0.7
 
-8-Jan-2014
+===29-Jul-2014
+
+- add format money
+
+==version 0.6
+
+===8-Jan-2014
 
 - permissions for https://www.amazon.com, for cart access
 - add some price block for transfer to rubles
--- script.js
+++ script.js
@@ -5,110 +5,133 @@
  * Time: 11:57 AM
  */
 (function($){
-	"use strict";
-	var APP = {};
+  "use strict";
+  var APP = {};
 
-	APP.core = function() {
-		var currentDate = '',
-			currentRate = '',
+  APP.core = function() {
+    var currentDate = '',
+      currentRate = '',
 
-			$priceSelectors = [
-				'.priceLarge',
-				'.price',
-				'.ourprice',
-				'.s9Price',
-				'.bld.lrg.red',
-				'.pa_priceTest',
-				'.hlb-price',
-				'.a-color-price',
-				'#price td.a-span12',
-				'.listprice',
-				'.offer-price'
-			].join(','),
+      $priceSelectors = [
+        '.priceLarge',
+        '.price',
+        '.ourprice',
+        '.s9Price',
+        '.bld.lrg.red',
+        '.pa_priceTest',
+        '.hlb-price',
+        '.a-color-price',
+        '#price td.a-span12',
+        '.listprice',
+        '.offer-price'
+      ].join(','),
 
-			insertPrice = function () {
-				$($priceSelectors).each(function () {
-					var price = parseFloat($(this).text().replace('$', "").replace(",","")),
-						newPrice = Math.round(price * currentRate);
-					if (!isNaN(newPrice)) {
-						this.innerHTML += '<span class="rubles">( ' + newPrice + ' руб.)</div>';
-					}
-				});
-			},
+      moneyFormat = function(num) {
+          var strNum = '' + parseFloat(num).toFixed(2),
+              parts = strNum.split('.'),
+              head = parts[0] || '',
+              tail = parts[1] || '',
+              s = [];
+          if (head) {
+            var l = head.length,
+                p = Math.floor(l / 3);
+            for (var i = 0; i < p; i++) {
+              s.push(head.substr(l - (i * 3) - 3, 3));
+            }
+            s.push(head.substr(0, (l % 3)));
+            s.reverse();
+          }
+          if (s.length > 0) {
+            return s.join(' ') + '.' + tail;
+          } else {
+            return num;
+          }
+      },
 
-			setCurrentRate = function (xml) {
-				if (!xml) {
-					console.log('empty xml. Fail.');
-					return;
-				}
-				var usd = $(xml).find('Valute[ID="R01235"] Value').html(),
-					rate = usd.replace(',', '.');
-				currentRate = rate;
-				localStorage.setItem('rate', rate);
-				localStorage.setItem('date', currentDate);
-				insertPrice();
-			},
+      insertPrice = function () {
+        $($priceSelectors).each(function () {
+          var price = parseFloat($(this).text().replace('$', "").replace(",","")),
+            newPrice = Math.round(price * currentRate);
+          if (!isNaN(newPrice)) {
+            this.innerHTML += '<span class="rubles">( ' +
+                moneyFormat(newPrice) + ' руб.)</div>';
+          }
+        });
+      },
 
-			ajax = function (url) {
-				var xhr = new XMLHttpRequest();
-				xhr.open("GET", url, true);
-				xhr.onreadystatechange = function () {
-					if (xhr.readyState === 4) {
-						setCurrentRate(xhr.responseText);
-					}
-				};
-				xhr.send();
-			},
+      setCurrentRate = function (xml) {
+        if (!xml) {
+          console.log('empty xml. Fail.');
+          return;
+        }
+        var usd = $(xml).find('Valute[ID="R01235"] Value').html(),
+          rate = usd.replace(',', '.');
+        currentRate = rate;
+        localStorage.setItem('rate', rate);
+        localStorage.setItem('date', currentDate);
+        insertPrice();
+      },
 
-			updatePrice = function () {
-				$('.rubles').remove();
-				insertPrice();
-			},
+      ajax = function (url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            setCurrentRate(xhr.responseText);
+          }
+        };
+        xhr.send();
+      },
 
-			update = function () {
-				var now = new Date().getTime();
-				if (lastUpdate + 2000 < now) {
-					setTimeout(function () {
-						updatePrice();
-					}, 2500);
-					lastUpdate = now;
-				}
-			},
+      updatePrice = function () {
+        $('.rubles').remove();
+        insertPrice();
+      },
 
-			lastUpdate = new Date().getTime(),
+      update = function () {
+        var now = new Date().getTime();
+        if (lastUpdate + 2000 < now) {
+          setTimeout(function () {
+            updatePrice();
+          }, 2500);
+          lastUpdate = now;
+        }
+      },
 
-			init = function() {
-				var today = new Date(),
-					d = today.getDate(),
-					m = today.getMonth()+ 1,
-					y = today.getFullYear();
-				currentDate = d + '-' + m + '-' + y;
-				if (localStorage.getItem('date') !== currentDate) {
-					var cUrl = "http://www.cbr.ru/scripts/XML_daily.asp";
-					ajax(cUrl);
-				} else {
-					currentRate = localStorage.getItem('rate');
-					insertPrice();
-				}
-			};
+      lastUpdate = new Date().getTime(),
 
-		return {
-			init: init,
-			update : update
-		};
-	}();
+      init = function() {
+        var today = new Date(),
+          d = today.getDate(),
+          m = today.getMonth()+ 1,
+          y = today.getFullYear();
+        currentDate = d + '-' + m + '-' + y;
+        if (localStorage.getItem('date') !== currentDate) {
+          var cUrl = "http://www.cbr.ru/scripts/XML_daily.asp";
+          ajax(cUrl);
+        } else {
+          currentRate = localStorage.getItem('rate');
+          insertPrice();
+        }
+      };
 
-	$(document).ready(function() {
-		setTimeout(function () {
-			APP.core.init();
-		}, 700);
-		setTimeout( function () {
-			document.addEventListener("DOMSubtreeModified", function(event){
-				APP.core.update();
-			});
-		},10000);
+    return {
+      init: init,
+      update : update
+    };
+  }();
 
-	});
+  $(document).ready(function() {
+    setTimeout(function () {
+      APP.core.init();
+    }, 700);
+    setTimeout( function () {
+      document.addEventListener("DOMSubtreeModified", function(event){
+        APP.core.update();
+      });
+    },10000);
+
+  });
 
 })($);
 
